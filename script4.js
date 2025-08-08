a9e5774a510a57a0f59521e92bf875e62255efc4
MorrissGrey <venancio4842@gmail.com>
2025-08-07 20:08:29 -0600

    actualización
    cambio de carpetas

--- script4.js
+++
@@ -1,8 +0,0 @@
-// Mostrar/ocultar detalles del proyecto
-document.querySelectorAll('.ver-mas').forEach(boton => {
-    boton.addEventListener('click', () => {
-        const detalles = boton.nextElementSibling;
-        detalles.classList.toggle('oculto');
-        boton.textContent = detalles.classList.contains('oculto') ? 'Ver más' : 'Ver menos';
-    });
-});
